4beeff8aec7bcabb770e248ffa60ea8246b7a116
dptd <dptdescribe@gmail.com>
2016-11-07 22:17:12 +0100

    partial fix for #3

--- chat.lisp
+++ chat.lisp
@@ -28,7 +28,7 @@
                                                 ((:fallback fallback))
                                                 ((:fallback_id fallback-id))
                                                 ((:attachment_type attachment-type))
-                                                ((:actions actions)) ;; todo, json
+                                                ;((:actions actions)) ;; todo, json
                                                 ((:color color))
                                                 ((:pretext pretext))
                                                 ((:author_name author-name))
@@ -37,20 +37,21 @@
                                                 ((:title title))
                                                 ((:title_link title-link))
                                                 ((:text text))
-                                                ((:fields fields)) ;; todo, json
+                                                ;((:fields fields)) ;; todo, json
                                                 ((:mrkdwn_in mrkdwn-in)) ;; todo, json
                                                 ((:image_url image-url))
                                                 ((:thumb_url thumb-url))
                                                 ((:footer footer))
                                                 ((:footer_icon footer_icon))
                                                 ((:ts ts)))
-  "Returns attachments which can be used in  post-message function."
-  (format nil "[~A]" (cl-json:encode-json-alist-to-string
-                      (jasa.core:prepare-arguments attachments
-                                         #'(lambda (parameter value)
-                                             (cons
-                                              (string-downcase parameter)
-                                              value))))))
+  "Returns attachments which can be used in post-message function."
+  (format nil "[{~A}]" (prepare-json-from-attachments attachments)))
+
+(defun prepare-json-from-attachments (attachments)
+  (if attachments
+      (if (eq (car attachments) :mrkdwn_in)
+          (concatenate 'string (format nil "\"mrkdwn_in\":[\"~A\"]" (cadr attachments)))
+          (concatenate 'string (format nil "\"~A\":\"~A\"," (string-downcase (car attachments)) (cadr attachments)) (foo (cddr attachments))))))
 
 (defun delete-message (&rest arguments &key
                                           ((:token token))
